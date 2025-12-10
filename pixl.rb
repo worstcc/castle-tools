1f2ec369980adff02600b350d4daa96f9ee0dba0
WorstCC <worstcc@proton.me>
2025-12-10 16:44:01 -0500

    Largely finish pixl

--- pixl.rb
+++ pixl.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/env ruby
+=begin
+pixl.swf structure: shape is placed by sprite which sets _visible to false, fixes in-game visual bugs caused by reference sometimes
+research importing image missing pixels near top left
+research alpha
+extracting ps3 pixl tags doesn't work
+add BREC mode for import/export (PIXL instead of LXIP)
+=end
 require 'rmagick'
 require 'pathname'
 require 'tempfile'
 require 'tmpdir'
 require 'nokogiri'
+require 'optparse'
 include Magick
 
 def findFFDec
@@ -18,7 +26,7 @@ def findFFDec
       ffdecPath = `which ffdec`.strip
     end
   end
-  
+
   if ffdecPath.nil? || !File.exist?(ffdecPath)
     puts "error: JPEXS not found"
     exit 0
@@ -27,166 +35,210 @@ def findFFDec
   end
 end
 
-# options = {}
-# OptionParser.new do |opts|
-#   opts.banner = "usage: #{File.basename($0)} [options] [image]"
-# end.parse!
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "usage: #{File.basename($0)} [image|swf] [output directory]"
+  opts.on("-r","--export-raw","use raw image dimensions in export") { options[:exportraw] = true }
+  opts.on("-d","--dry-run","don't write to files"){ options[:dryrun] = true }
+end.parse!
 
 if ARGV.length != 2
-  puts "usage: #{File.basename($0)} [image] [swf]"
+  puts "usage: #{File.basename($0)} [image|swf] [output directory]"
   exit 1
 end
 
-imageFile = ARGV[0]
-if ! File.exist?(imageFile)
-  raise "#{File.basename(imageFile)} not found"
+inputFile = Pathname.new(ARGV[0])
+if ! File.exist?(inputFile)
+  raise "#{File.basename(inputFile)} not found"
 end
-if ! [".jpg",".jpeg",".png"].include?(File.extname(imageFile).downcase)
-  raise "#{File.basename(imageFile)} is not a jpg|jpeg|png"
+if ! [".png",".swf"].include?(File.extname(inputFile).downcase)
+  raise "#{File.basename(inputFile)} is not a png|swf"
 end
 
-swfFile = ARGV[1]
-if ! File.exist?(swfFile)
-  raise "#{File.basename(swfFile)} not found"
-end
-if File.extname(swfFile).downcase != ".swf"
-  raise "#{File.basename(swfFile)} is not a swf"
+outputDir = Pathname.new(ARGV[1])
+if ! Dir.exist?(outputDir)
+  raise "#{outputDir} not found"
 end
 
 ffdec = findFFDec
 
-# read image
-image = Image.read(imageFile).first
-if image.columns > 1024 || image.rows > 1024
-  raise "#{File.basename(imageFile)} is too large (maximum 1024x1024)"  
-end
-imageData = image.export_pixels(0,0,image.columns,image.rows,"BGRA").pack('C*').unpack('H*').first
+if File.extname(inputFile).downcase == ".swf"
+  # extract images from swf pixl tags
 
-# create pixl tag
+  # get xml
+  swfXml = File.join(Pathname.new(Dir.tmpdir) + "temp.xml")
+  system(ffdec,'-swf2xml',inputFile.to_s,swfXml.to_s)
+  doc = Nokogiri::XML(File.read(swfXml)) { |config| config.huge }
 
-# SWF header
+  # collect unknown tags
+  doc.xpath("//tags/*").each do |tag|
+    if tag["unknownData"]
+      data = tag["unknownData"]
+      pixlHeaderIndex = data.index("4c584950")
+      if pixlHeaderIndex
+        # check if footer is valid
+        if data[-32,32] != "24000000280000003000000003000000"
+          puts "error: invalid pixl tag"
+          next
+        end
+        # get pixl data
+        columns = [data[pixlHeaderIndex + 32,8]].pack("H*").unpack1("V")
+        rows = [data[pixlHeaderIndex + 40,8]].pack("H*").unpack1("V")
+        placedColumns = [data[16,4]].pack("H*").unpack1("v") / 10
+        placedRows = [data[32,4]].pack("H*").unpack1("v") / 10
+        id = [data[pixlHeaderIndex + 160,4]].pack("H*").unpack1("v")
 
-data = ""
-# character ID
-data << [49002].pack('v').unpack1('H*')
-data << "00" * 2
-# width
-data << [65536 - image.columns * 10].pack('v').unpack1('H*')
-data << "ff" * 2
-data << [image.columns * 10].pack('v').unpack1('H*')
-data << "00" * 2
-# height
-data << [65536 - image.rows * 10].pack('v').unpack1('H*')
-data << "ff" * 2
-data << [image.rows * 10].pack('v').unpack1('H*')
-data << "00" * 2
-# padding
-data << "cd" * 16
+        # placed dimensions aren't in same order all the time, making the higher number the columns seems to work
+        if placedRows > placedColumns
+          placedColumns, placedRows = placedRows, placedColumns
+        end
 
-# PIXL header
-data << "4c584950"
-data << "00" * 12
-# width
-data << [image.columns].pack('V').unpack1('H*')
-# height
-data << [image.rows].pack('V').unpack1('H*')
-data << "01000000" * 2
-data << "00" * 4
-# ?
-data << "60000000"
-data << "00" * 4
-data << "01000000"
-# image length
-data << [imageData.length / 2].pack('V').unpack1('H*')
-data << "01000000" * 2
-# width/height/width/height
-data << [image.columns].pack('V').unpack1('H*')
-data << [image.rows].pack('V').unpack1('H*')
-data << [image.columns].pack('V').unpack1('H*')
-data << [image.rows].pack('V').unpack1('H*')
-data << "cd" * 4
-# character ID
-data << [49002].pack('v').unpack1('H*')
-data << "00" * 14
+        # get correct placed values
+        if placedColumns != columns
+          placedColumns = placedColumns / 2 + columns
+        end
+        if placedRows != rows
+          placedRows = placedRows / 2 + rows
+        end
+        puts "(#{id}) raw: #{columns}x#{rows}, placed: #{placedColumns}x#{placedRows}"
 
-# image data
-data << imageData
+        byteLength = [data[pixlHeaderIndex + 96,103]].pack("H*").unpack1("V")
+        imageData = data[pixlHeaderIndex + 192,byteLength * 2]
 
-# footer
-data << "24000000280000003000000003000000"
+        # remove "cdcdcdcd" bytes from image data
+        imageData = imageData.sub(/\A(cdcdcdcd)+/,"")
+        imageData = [imageData].pack("H*")
 
-scriptDir = Pathname.new(__FILE__).realpath.parent
-pixlSWF = scriptDir + "swf" + "pixl.swf"
-unless pixlSWF.exist?
-  raise "swf/pixl.swf missing in script directory"
-end
-pixlSWFBak = Pathname.new(Dir.tmpdir) + "pixl.swf"
-pixlXML = Pathname.new(Dir.tmpdir) + "pixl.xml"
-FileUtils.cp(pixlSWF,pixlSWFBak)
-
-# import image
-system(ffdec,'-replace',pixlSWF.to_s,pixlSWF.to_s,'49000',imageFile)
-# get XML for editing shape & pixl tags
-system(ffdec,'-swf2xml',pixlSWF.to_s,pixlXML.to_s)
-pixlDoc = Nokogiri::XML(File.read(pixlXML)) { |config| config.huge }
-
-# edit XML
-tags = pixlDoc.at_xpath("//tags")
-# shape
-shape = tags.at_xpath("//item[@type='DefineShapeTag']")
-shapeBounds = shape.at_xpath("//shapeBounds")
-shapeBounds["Xmin"] = image.columns * -10;
-shapeBounds["Xmax"] = image.columns * 10;
-shapeBounds["Ymin"] = image.rows * -10;
-shapeBounds["Ymax"] = image.rows * 10;
-bitmapMatrix = shape.at_xpath("//shapes/fillStyles/fillStyles/item[last()]/bitmapMatrix")
-bitmapMatrix["translateX"] = image.columns * -10
-bitmapMatrix["translateY"] = image.rows * -10
-styleChangeRecord = shape.at_xpath("//shapes/shapeRecords/item[@type='StyleChangeRecord']")
-styleChangeRecord["moveDeltaX"] = image.columns * -10
-styleChangeRecord["moveDeltaY"] = image.rows * -10
-straightEdgeRecords = shape.xpath("//shapes/shapeRecords/item[@type='StraightEdgeRecord']")
-straightEdgeRecords[0]["deltaX"] = image.columns * 20
-straightEdgeRecords[1]["deltaY"] = image.rows * 20
-straightEdgeRecords[2]["deltaX"] = image.columns * -20
-straightEdgeRecords[3]["deltaY"] = image.rows * -20
-# pixl
-tags.at_xpath("//item[@type='UnknownTag']")["unknownData"] = data
-
-# copy pixl tags to SWF file
-
-# get SWF XML
-swfXML = Pathname.new(Dir.tmpdir) + "swf.xml"
-system(ffdec,'-swf2xml',swfFile.to_s,swfXML.to_s)
-swfDoc = Nokogiri::XML(File.read(swfXML)) { |config| config.huge }
-
-# collect pixl tags
-pixlXMLContent = []
-found = false
-pixlDoc.xpath('/swf/tags/item').each do |node|
-  if node['type'] == 'SetBackgroundColorTag'
-    found = true
-    next
+        # export image
+        image = Image.new(columns,rows)
+        image.import_pixels(0,0,columns,rows,"BGRA",imageData,CharPixel)
+        if ! options[:exportraw]
+          image = image.resize(placedColumns,placedRows)
+        end
+        if ! options[:dryrun]
+          image.write("png:" + File.join(outputDir + "#{id}.png"))
+        end
+      end
+    end
   end
-  break if node['type'] == 'ShowFrameTag'
-  pixlXMLContent << node if found
+
+  FileUtils.rm(swfXml)
+else 
+  # import image into copy of pixl swf
+
+  # read image
+  image = Image.read(inputFile).first
+  # if image.depth != 8
+  #   raise "#{File.basename(inputFile)} depth is not 8-bit"
+  # end
+  if image.columns > 1024 || image.rows > 1024
+    raise "#{File.basename(inputFile)} is too large (maximum 1024x1024)"  
+  end
+  imageData = image.export_pixels(0,0,image.columns,image.rows,"BGRA").pack('C*').unpack('H*').first
+
+  # create pixl tag
+
+  # swf header
+
+  data = ""
+  # character ID
+  data << [65534].pack('v').unpack1('H*')
+  data << "00" * 2
+  # width
+  data << [65536 - image.columns * 10].pack('v').unpack1('H*')
+  data << "ff" * 2
+  data << [image.columns * 10].pack('v').unpack1('H*')
+  data << "00" * 2
+  # height
+  data << [65536 - image.rows * 10].pack('v').unpack1('H*')
+  data << "ff" * 2
+  data << [image.rows * 10].pack('v').unpack1('H*')
+  data << "00" * 2
+  # padding
+  data << "cd" * 16
+
+  # PIXL header
+  data << "4c584950"
+  data << "00" * 12
+  # width
+  data << [image.columns].pack('V').unpack1('H*')
+  # height
+  data << [image.rows].pack('V').unpack1('H*')
+  data << "01000000" * 2
+  data << "00" * 4
+  # ?
+  data << "60000000"
+  data << "00" * 4
+  data << "01000000"
+  # image length
+  data << [imageData.length / 2].pack('V').unpack1('H*')
+  data << "01000000" * 2
+  # width/height/width/height
+  data << [image.columns].pack('V').unpack1('H*')
+  data << [image.rows].pack('V').unpack1('H*')
+  data << [image.columns].pack('V').unpack1('H*')
+  data << [image.rows].pack('V').unpack1('H*')
+  data << "cd" * 4
+  # character ID
+  data << [65534].pack('v').unpack1('H*')
+  data << "00" * 14
+
+  # image data
+  data << imageData
+
+  # footer
+  data << "24000000280000003000000003000000"
+
+  # import image
+
+  scriptDir = Pathname.new(__FILE__).realpath.parent
+  pixlSwf = File.join(scriptDir + "swf" + "pixl.swf")
+  unless File.exist?(pixlSwf)
+    raise "swf/pixl.swf missing in script directory"
+  end
+  swfXml = File.join(Pathname.new(Dir.tmpdir) + "temp.xml")
+  outputSwf = File.join(Pathname.new(Dir.tmpdir) + "temp.swf")
+  FileUtils.cp(pixlSwf,outputSwf)
+
+  system(ffdec,'-replace',outputSwf.to_s,outputSwf.to_s,"65532",inputFile.to_s)
+  # get XML for editing shape & pixl tags
+  system(ffdec,'-swf2xml',outputSwf.to_s,swfXml.to_s)
+  doc = Nokogiri::XML(File.read(swfXml)) { |config| config.huge }
+
+  # edit XML
+  tags = doc.at_xpath("//tags")
+  # shape
+  shape = tags.at_xpath("//item[@type='DefineShapeTag']")
+  shapeBounds = shape.at_xpath("//shapeBounds")
+  shapeBounds["Xmin"] = image.columns * -10;
+  shapeBounds["Xmax"] = image.columns * 10;
+  shapeBounds["Ymin"] = image.rows * -10;
+  shapeBounds["Ymax"] = image.rows * 10;
+  bitmapMatrix = shape.at_xpath("//shapes/fillStyles/fillStyles/item[last()]/bitmapMatrix")
+  bitmapMatrix["translateX"] = image.columns * -10
+  bitmapMatrix["translateY"] = image.rows * -10
+  styleChangeRecord = shape.at_xpath("//shapes/shapeRecords/item[@type='StyleChangeRecord']")
+  styleChangeRecord["moveDeltaX"] = image.columns * -10
+  styleChangeRecord["moveDeltaY"] = image.rows * -10
+  straightEdgeRecords = shape.xpath("//shapes/shapeRecords/item[@type='StraightEdgeRecord']")
+  straightEdgeRecords[0]["deltaX"] = image.columns * 20
+  straightEdgeRecords[1]["deltaY"] = image.rows * 20
+  straightEdgeRecords[2]["deltaX"] = image.columns * -20
+  straightEdgeRecords[3]["deltaY"] = image.rows * -20
+  # pixl
+  tags.at_xpath("//item[@type='UnknownTag']")["unknownData"] = data
+
+  File.write(swfXml,doc.to_xml(indent:2,indent_text:"  ").gsub(/<\/item><item/,"</item>\n  <item"))
+
+  # create output swf in output directory
+  if ! options[:dryrun]
+    system(ffdec,'-xml2swf',swfXml.to_s,outputSwf.to_s)
+    FileUtils.cp(outputSwf,File.join(outputDir,File.basename(inputFile,File.extname(inputFile)) + ".swf"))
+  end
+
+  FileUtils.rm(swfXml)
+  FileUtils.rm(outputSwf)
 end
-
-# import pixl tags to SWF
-swfXMLInsertNode = swfDoc.at_xpath("/swf/tags/item[@type='SetBackgroundColorTag']")
-pixlXMLContent.each do |node|
-  new = node.dup
-  swfXMLInsertNode.add_next_sibling(new)
-  swfXMLInsertNode = new
-end
-
-File.write(swfXML.to_s,swfDoc.to_xml(indent:2,indent_text:"  ").gsub(/<\/item><item/,"</item>\n  <item"))
-system(ffdec,'-xml2swf',swfXML.to_s,swfFile.to_s)
-
-FileUtils.cp(pixlSWFBak,pixlSWF)
-FileUtils.rm(pixlSWFBak)
-FileUtils.rm(pixlXML)
-FileUtils.rm(swfXML)
 
 =begin
 shape notes

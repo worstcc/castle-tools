678016614202f8f9851c4e1d9e432ec63bd345a6
WorstCC <worstcc@proton.me>
2026-03-20 17:55:08 -0400

    feat(crypt): use temp files, less caps, fix bsp

--- crypt.rb
+++ crypt.rb
@@ -44,16 +44,16 @@ def zipFile(outFile,archiveName,data)
 end
 
 def getBlowfishKey(name,dataLength)
-  nameCRC = 0
-  name.each_byte { |b| nameCRC = ((nameCRC * 0x25) + b) & 0xffffffff }
+  nameCrc = 0
+  name.each_byte { |b| nameCrc = ((nameCrc * 0x25) + b) & 0xffffffff }
 
   sizeTemp = ((dataLength / 16) % 16).to_i
   keyIndexes = KEYSWAPINDEXES[sizeTemp / 4]
   key = KEYSTRINGS[sizeTemp % 4].byteslice(0,18).bytes
 
   keyIndexes.each do |i|
-    key[i] = nameCRC & 0xff
-    nameCRC >>= 8
+    key[i] = nameCrc & 0xff
+    nameCrc >>= 8
   end
 
   key.pack('C*')
@@ -77,8 +77,8 @@ def blowfishCrypt(name,data,mode)
     block = data[i * 8, 8]
     # reverse bytes within each 32-bit word
     blockBytes = block.bytes
-    blockLE = (blockBytes[0,4].reverse + blockBytes[4,4].reverse).pack('C*')
-    encryptedBlock = cipher.update(blockLE)
+    blockLe = (blockBytes[0,4].reverse + blockBytes[4,4].reverse).pack('C*')
+    encryptedBlock = cipher.update(blockLe)
     # reverse output bytes within each 32-bit word
     encryptedBlockBytes = encryptedBlock.bytes
     encrypted << (encryptedBlockBytes[0,4].reverse + encryptedBlockBytes[4,4].reverse).pack('C*')
@@ -103,7 +103,7 @@ def validateChecksum(data)
   abort format('error: checksum mismatch: %<ck1>08X != %<ck2>08X',ck1:ck1,ck2:ck2) if ck1 != ck2
 end
 
-def buildHeaderNREC(data)
+def buildHeaderNrec(data)
   ret = Array.new(0x80,0)
   ret[0,4] = '6KOC'.bytes
   ret[0x10,4] = [data.bytesize].pack('V').bytes
@@ -111,11 +111,11 @@ def buildHeaderNREC(data)
   ret.pack('C*')
 end
 
-def buildFooterNREC(data)
+def buildFooterNrec(data)
   ret = []
   # add initial padding to align the file to 4 bytes
-  align4Length = (4 - (data.bytesize % 4)) % 4
-  ret.concat([0x00] * align4Length)
+  alignFourLength = (4 - (data.bytesize % 4)) % 4
+  ret.concat([0x00] * alignFourLength)
   # add 0x14, 1
   ret.concat([0x14,0x00,0x00,0x00,0x01,0x00,0x00,0x00])
   # add padding to align the final file to 16 bytes (existing data, footer so far, int for number of zeroes, int for checksum)
@@ -129,38 +129,38 @@ def buildFooterNREC(data)
   ret.pack('C*')
 end
 
-def decryptSWFNREC(data)
+def decryptSwfNrec(data)
   size = data[0x10,4].unpack1('V')
   offset = data[0x14,4].unpack1('V')
   data[offset,size]
 end
 
-def decryptNREC(inFile,outDir)
-  puts "decrypting #{File.basename(inFile)} (NREC)"
+def decryptNrec(inFile,outDir)
+  puts "decrypting #{File.basename(inFile)} (nrec)"
   name, inputData = unzipFile(inFile)
   decrypted = blowfishCrypt(name,inputData,'decrypt')
   # sanity checks: validate checksum, confirm we can properly rebuild the footer
   validateChecksum(decrypted)
-  swfData = decryptSWFNREC(decrypted)
-  newSWFData = swfData
+  swfData = decryptSwfNrec(decrypted)
+  newSwfData = swfData
 
   # remove "CD" bytes and everything after
   pattern = [0x40,0x00,0x00,0x00,0xCD].pack('C*').force_encoding('ASCII-8BIT')
-  newSWFData.force_encoding('ASCII-8BIT')
-  cdIndex = newSWFData.rindex(pattern)
-  newSWFData = newSWFData[0,cdIndex + 4] if cdIndex
-  # update SWF header file length
-  newLength = newSWFData.length
-  newSWFData = newSWFData[0,4] + [newLength].pack('V') + newSWFData[8..]
+  newSwfData.force_encoding('ASCII-8BIT')
+  cdIndex = newSwfData.rindex(pattern)
+  newSwfData = newSwfData[0,cdIndex + 4] if cdIndex
+  # update swf header file length
+  newLength = newSwfData.length
+  newSwfData = newSwfData[0,4] + [newLength].pack('V') + newSwfData[8..]
 
-  File.write(File.join(outDir,"#{name.split('.')[0]}.swf"),newSWFData,mode:'wb')
+  File.write(File.join(outDir,"#{name.split('.')[0]}.swf"),newSwfData,mode:'wb')
 end
 
-def encryptNREC(inFile,outDir)
-  puts "encrypting #{File.basename(inFile)} (NREC)"
+def encryptNrec(inFile,outDir)
+  puts "encrypting #{File.basename(inFile)} (nrec)"
   swfData = File.binread(inFile)
-  preData = buildHeaderNREC(swfData) + swfData
-  preData += buildFooterNREC(preData)
+  preData = buildHeaderNrec(swfData) + swfData
+  preData += buildFooterNrec(preData)
   checksum = calcChecksum(preData)
   preData[-4,4] = [checksum].pack('V')
   archiveName = "#{File.basename(inFile,'.swf').upcase}.COK6.NREC"
@@ -169,7 +169,7 @@ def encryptNREC(inFile,outDir)
   zipFile(zipPath,archiveName,encrypted)
 end
 
-def buildHeaderBREC(data)
+def buildHeaderBrec(data)
   ret = Array.new(0x80,0)
   ret[0,4] = 'COK6'.bytes
   ret[0x10,4] = [data.bytesize].pack('N').bytes
@@ -177,67 +177,66 @@ def buildHeaderBREC(data)
   ret.pack('C*')
 end
 
-def buildFooterBREC(data)
+def buildFooterBrec(data)
   ret = [0xCD,0xCD]
   # add initial padding to align the file to 4 bytes
-  align4Length = (4 - ((data.bytesize + 2) % 4)) % 4
-  ret.concat([0x00] * align4Length)
+  alignFourLength = (4 - ((data.bytesize + 2) % 4)) % 4
+  ret.concat([0x00] * alignFourLength)
   # add 0x14, 1
   ret.concat([0x00,0x00,0x00,0x14,0x00,0x00,0x00,0x01])
   ret.pack('C*')
 end
 
-def decryptSWFBREC(data)
+def decryptSwfBrec(data)
   size = data[0x10,4].unpack1('N')
   offset = data[0x14,4].unpack1('N')
   data[offset,size]
 end
 
-def decryptBREC(inFile,outDir)
-  puts "decrypting #{File.basename(inFile)} (BREC)"
+def decryptBrec(inFile,outDir)
+  puts "decrypting #{File.basename(inFile)} (brec)"
   name, inputData = unzipFile(inFile)
-  swfData = decryptSWFBREC(inputData)
-  newSWFData = swfData
+  swfData = decryptSwfBrec(inputData)
+  newSwfData = swfData
   # remove "CD" bytes and everything after
   pattern = [0x40,0x00,0x00,0x00,0xCD].pack('C*').force_encoding('ASCII-8BIT')
-  newSWFData.force_encoding('ASCII-8BIT')
-  cdIndex = newSWFData.rindex(pattern)
-  newSWFData = newSWFData[0,cdIndex + 4] if cdIndex
-  # update SWF header file length
-  newLength = newSWFData.length
-  newSWFData = newSWFData[0,4] + [newLength].pack('N') + newSWFData[8..]
-
-  File.write(File.join(outDir,"#{name.split('.')[0]}.swf"),newSWFData,mode:'wb')
+  newSwfData.force_encoding('ASCII-8BIT')
+  cdIndex = newSwfData.rindex(pattern)
+  newSwfData = newSwfData[0,cdIndex + 4] if cdIndex
+  # update swf header file length
+  newLength = newSwfData.length
+  newSwfData = newSwfData[0,4] + [newLength].pack('N') + newSwfData[8..]
+  File.write(File.join(outDir,"#{name.split('.')[0]}.swf"),newSwfData,mode:'wb')
 end
 
-def encryptBREC(inFile,outDir)
-  puts "encrypting #{File.basename(inFile)} (BREC)"
+def encryptBrec(inFile,outDir)
+  puts "encrypting #{File.basename(inFile)} (brec)"
   swfData = File.binread(inFile)
-  preData = buildHeaderBREC(swfData) + swfData
-  preData += buildFooterBREC(preData)
+  preData = buildHeaderBrec(swfData) + swfData
+  preData += buildFooterBrec(preData)
   archiveName = "#{File.basename(inFile,'.swf').upcase}.COK6.BREC"
   zipPath = outDir + "/#{File.basename(inFile, '.*').downcase}.pak"
   zipFile(zipPath,archiveName,preData)
 end
 
-def padPDAG(data,blockSize = 8)
+def padPdag(data,blockSize = 8)
   totalLength = data.bytesize + 4
   padLength = (blockSize - (totalLength % blockSize)) % blockSize
   data + ("\x00" * padLength)
 end
 
-def decryptPDAGNREC(inFile,outDir)
-  puts "decrypting #{File.basename(inFile)} (NREC)"
+def decryptPdagNrec(inFile,outDir)
+  puts "decrypting #{File.basename(inFile)} (nrec)"
   name, inputData = unzipFile(inFile)
   decrypted = blowfishCrypt(name,inputData,'decrypt')
   decrypted = decrypted.byteslice(0...-8) # remove checksum footer
   File.write(File.join(outDir,"#{name.split('.')[0]}.pdag"),decrypted,mode:'wb')
 end
 
-def encryptPDAGNREC(inFile,outDir)
-  puts "encrypting #{File.basename(inFile)} (NREC)"
+def encryptPdagNrec(inFile,outDir)
+  puts "encrypting #{File.basename(inFile)} (nrec)"
   data = File.binread(inFile)
-  preData = padPDAG(data)
+  preData = padPdag(data)
   checksum = calcChecksum("#{preData}\x00\x00\x00\x00")
   preData += [checksum].pack('V')
   archiveName = "#{File.basename(inFile,'.pdag').upcase}.PDAG.NREC"
@@ -246,38 +245,38 @@ def encryptPDAGNREC(inFile,outDir)
   zipFile(zipPath,archiveName,encrypted)
 end
 
-def buildHeaderPDAGNREC(data)
+def buildHeaderPdagNrec(data)
   ret = Array.new(0x14,0)
   ret[0,4] = 'GADP'.bytes
   ret[0x10,4] = [data].pack('V').bytes
   ret.pack('C*')
 end
 
-def decryptPDAGBREC(inFile,outDir)
-  puts "decrypting #{File.basename(inFile)} (BREC)"
+def decryptPdagBrec(inFile,outDir)
+  puts "decrypting #{File.basename(inFile)} (brec)"
   name,data = unzipFile(inFile)
   File.write(File.join(outDir,"#{name.split('.')[0]}.pdag"),data)
 end
 
-def encryptPDAGBREC(inFile,outDir)
-  puts "encrypting #{File.basename(inFile)} (BREC)"
+def encryptPdagBrec(inFile,outDir)
+  puts "encrypting #{File.basename(inFile)} (brec)"
   data = File.binread(inFile)
-  data = padPDAG(data)
+  data = padPdag(data)
   archiveName = "#{File.basename(inFile,'.pdag').upcase}.PDAG.BREC"
   zipPath = outDir + "/#{File.basename(inFile,'.*').downcase}.pak"
   zipFile(zipPath,archiveName,data)
 end
 
-def buildHeaderPDAGBREC(data)
+def buildHeaderPdagBrec(data)
   ret = Array.new(0x14,0)
   ret[0,4] = 'PDAG'.bytes
   ret[0x10,4] = [data].pack('N').bytes
   ret.pack('C*')
 end
 
-def createBSP(inFile,outDir,bspName)
-  bspSWF = File.join(__dir__,'swf','bsp.swf')
-  abort 'error: bsp/bsp.swf missing in script directory' unless File.exist?(bspSWF)
+def createBsp(inFile,outDir,bspName,options)
+  bspSwf = File.join(__dir__,'swf','bsp.swf')
+  abort 'error: bsp/bsp.swf missing in script directory' unless File.exist?(bspSwf)
 
   # get ruffle & FFDec
   ffdec = nil
@@ -292,16 +291,17 @@ def createBSP(inFile,outDir,bspName)
   abort 'error: ruffle is not installed on the system/not in PATH (download: https://ruffle.rs/downloads)' if ruffle.nil? || !File.exist?(ruffle)
   abort 'error: JPEXS is not installed on the system' if ffdec.nil? || !File.exist?(ffdec)
 
-  bspSWFBackup = File.join(Dir.tmpdir,'bspBackup.swf')
-  FileUtils.cp(bspSWF,bspSWFBackup)
+  prevBspSwf = Tempfile.new(['','.swf'])
+  FileUtils.cp(bspSwf,prevBspSwf)
   # get XML files
-  bspXML = File.join(Dir.tmpdir,'bsp.xml')
-  system(ffdec,'-swf2xml',bspSWF.to_s,bspXML.to_s)
-  levelXML = File.join(Dir.tmpdir,"#{File.basename(inFile,'.swf')}.xml")
-  system(ffdec,'-swf2xml',inFile,levelXML.to_s)
-  # copy level SWF contents to bsp.swf using XML
-  levelDoc = Nokogiri::XML(File.read(levelXML.to_s),nil,nil,Nokogiri::XML::ParseOptions::HUGE)
-  bspDoc = Nokogiri::XML(File.read(bspXML.to_s),nil,nil,Nokogiri::XML::ParseOptions::HUGE)
+  # bspXml = File.join(Dir.tmpdir,'bsp.xml')
+  bspXml = Tempfile.new(['','.xml'])
+  system(ffdec,'-swf2xml',bspSwf.to_s,bspXml.path)
+  levelXml = Tempfile.new(['','.xml'])
+  system(ffdec,'-swf2xml',inFile,levelXml.path)
+  # copy level swf contents to bsp.swf using XML
+  levelDoc = Nokogiri::XML(File.read(levelXml.path),nil,nil,Nokogiri::XML::ParseOptions::HUGE)
+  bspDoc = Nokogiri::XML(File.read(bspXml.path),nil,nil,Nokogiri::XML::ParseOptions::HUGE)
   foundGameObj = false
   filteredContent = []
   levelDoc.xpath('/swf/tags/item').each do |node|
@@ -318,26 +318,23 @@ def createBSP(inFile,outDir,bspName)
   end
   xmlOutput = bspDoc.to_xml(indent:2,indent_text:'  ')
   xmlOutput = xmlOutput.gsub(%r{</item><item},"</item>\n  <item")
-  File.write(bspXML.to_s,xmlOutput)
-  system(ffdec,'-xml2swf',bspXML.to_s,bspSWF.to_s)
-  # get BSP data from running bsp.swf
-  output,_stderr,_status = Open3.capture3(ruffle,'--scale','no-scale',bspSWF.to_s)
-  # clean up XML files & restore bsp.swf
-  FileUtils.rm(bspXML)
-  FileUtils.rm(levelXML)
-  FileUtils.cp(bspSWFBackup,bspSWF)
-  FileUtils.rm(bspSWFBackup)
+  File.write(bspXml.path,xmlOutput)
+  system(ffdec,'-xml2swf',bspXml.path,bspSwf.to_s)
+  # get bsp data from running bsp.swf
+  output,_stderr,_status = Open3.capture3(ruffle,'--scale','no-scale',bspSwf.to_s)
+  # restore bsp.swf
+  FileUtils.cp(prevBspSwf,bspSwf)
   # process ruffle output
   filteredOutput = []
   output.each_line do |line|
-    abort 'error: no BSP lines in input' if line.include?('error: no lines')
+    abort 'error: no bsp lines in input' if line.include?('error: no lines')
     break if line.include?('BSPEND')
 
     filteredOutput << line[78..] if line.include?('avm_trace')
   end
   abort 'error: no ruffle output (closed too early?)' if filteredOutput == []
 
-  # generate BSP arrays
+  # generate bsp arrays
   waypoints = false
   lineLength = 0
   lineData = []
@@ -359,7 +356,7 @@ def createBSP(inFile,outDir,bspName)
       lineData << line.to_f
     end
   end
-  bspData = options[:brec] ? buildHeaderPDAGBREC(lineLength) : buildHeaderPDAGNREC(lineLength)
+  bspData = options[:brec] ? buildHeaderPdagBrec(lineLength) : buildHeaderPdagNrec(lineLength)
   lineData.each do |value|
     floatValue = options[:brec] ? [value].pack('g') : [value].pack('e')
     bspData += floatValue
@@ -370,13 +367,13 @@ def createBSP(inFile,outDir,bspName)
   end
   # add extra zero bytes to prevent waypoint functions reading garbage data
   bspData += "\x00" * 2352
-  # extra bytes for NREC
+  # extra bytes for nrec
   bspData += "\x10\x00\x00\x00" unless options[:brec]
-  # write BSP file
+  # write bsp file
   pdag = File.join(Dir.tmpdir,"#{bspName}.pdag")
   File.write(pdag,bspData,mode:'wb')
-  # create BSP PAK file
-  options[:brec] ? encryptPDAGBREC(pdag,outDir) : encryptPDAGNREC(pdag,outDir)
+  # create bsp pak file
+  options[:brec] ? encryptPdagBrec(pdag,outDir) : encryptPdagNrec(pdag,outDir)
   FileUtils.rm(pdag)
 end
 
@@ -385,23 +382,24 @@ def decryptFile(inFile, outDir)
 
   Zip::File.open(inFile) do |zip|
     fileList = zip.entries.map(&:name)
-    isPDAG = fileList.any? { |f| f.include?('PDAG') }
-    hasNREC = fileList.any? { |f| f.include?('.NREC') }
-    hasBREC = fileList.any? { |f| f.include?('.BREC') }
-    if hasNREC
-      isPDAG ? decryptPDAGNREC(inFile,outDir) : decryptNREC(inFile,outDir)
-    elsif hasBREC
-      isPDAG ? decryptPDAGBREC(inFile,outDir) : decryptBREC(inFile,outDir)
+    isPdag = fileList.any? { |f| f.include?('PDAG') }
+    hasNrec = fileList.any? { |f| f.include?('.NREC') }
+    hasBrec = fileList.any? { |f| f.include?('.BREC') }
+    if hasNrec
+      isPdag ? decryptPdagNrec(inFile,outDir) : decryptNrec(inFile,outDir)
+    elsif hasBrec
+      isPdag ? decryptPdagBrec(inFile,outDir) : decryptBrec(inFile,outDir)
     end
   end
 end
+
 options = {}
 OptionParser.new do |opts|
   opts.banner = 'usage: crypt.rb [options]'
   opts.on('--encrypt','encrypt the input file instead of decrypting') { options[:encrypt] = true }
-  opts.on('--brec','when using --encrypt, output to BREC format (XBLA/PS3)') { options[:brec] = true }
-  opts.on('--bsp','create a BSP PAK file from input level SWF') { options[:bsp] = true }
-  opts.on('--bspname NAME',String,'BSP PAK file name when using --bsp option') { |name| options[:bspname] = name }
+  opts.on('--brec','when using --encrypt, output to brec format (xbla/ps3)') { options[:brec] = true }
+  opts.on('--bsp','create a bsp pak file from input level swf') { options[:bsp] = true }
+  opts.on('--bspname NAME',String,'bsp pak file name when using --bsp option') { |name| options[:bspname] = name }
 end.parse!
 
 if ARGV.length != 2
@@ -419,16 +417,16 @@ if options[:bsp]
   if options[:bspname]
     bspName = options[:bspname]
   else
-    print 'enter BSP name: '
+    print 'enter bsp name: '
     bspName = $stdin.gets.chomp
   end
   bspName = bspName.downcase
-  createBSP(inFile,outDir,bspName)
+  createBsp(inFile,outDir,bspName,options)
 elsif options[:encrypt]
   if inFile.end_with?('.pdag')
-    options[:brec] ? encryptPDAGBREC(inFile,outDir) : encryptPDAGNREC(inFile,outDir)
+    options[:brec] ? encryptPdagBrec(inFile,outDir) : encryptPdagNrec(inFile,outDir)
   else
-    options[:brec] ? encryptBREC(inFile,outDir) : encryptNREC(inFile,outDir)
+    options[:brec] ? encryptBrec(inFile,outDir) : encryptNrec(inFile,outDir)
   end
 else
   decryptFile(inFile,outDir)

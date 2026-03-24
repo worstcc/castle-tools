3739a99e13c6c6cbaa830d62e0c1486bc1041238
WorstCC <worstcc@proton.me>
2026-03-24 14:30:16 -0400

    feat(bsp): txt input support
    Providing a txt file that contains bsp data in the print format instead
    of a swf will create a bsp.

--- bsp.rb
+++ bsp.rb
@@ -6,7 +6,7 @@ require 'open3'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "usage: #{File.basename($PROGRAM_NAME)} [options] [input swf] [outdir]"
+  opts.banner = "usage: #{File.basename($PROGRAM_NAME)} [options] [input swf/txt file] [outdir]"
   opts.on('-b','--brec','use brec format (xbla/ps3)') { options[:brec] = true }
   opts.on('-nNAME','--name NAME',String,'name of pak file') { |name| options[:name] = name }
   opts.on('-u','--unbalanced',"don't balance bsp") { options[:unbalanced] = true }
@@ -17,13 +17,15 @@ end.parse!
 # get parameters, scripts, programs
 if options[:blank]
   outDir = ARGV[0]
-  abort "usage: #{File.basename($PROGRAM_NAME)} [options] [input swf] [outdir]" if ARGV.length != 1
+  abort "usage: #{File.basename($PROGRAM_NAME)} [options] [input swf/txt file] [outdir]" if ARGV.length != 1
 else
-  abort "usage: #{File.basename($PROGRAM_NAME)} [options] [input swf] [outdir]" if ARGV.length != 2
-  swf = File.expand_path(ARGV[0])
-  outDir = ARGV[1]
-  abort "error: file '#{swf}' does not exist" unless File.exist?(swf)
-  abort "error: '#{swf}' is not a .swf file" unless swf.downcase.end_with?('.swf')
+  abort "usage: #{File.basename($PROGRAM_NAME)} [options] [input swf/txt file] [outdir]" if ARGV.length != 2
+  inputFile = File.expand_path(ARGV[0])
+  outDir = File.expand_path(ARGV[1])
+  abort "error: file '#{File.basename(inputFile)}' does not exist" unless File.exist?(inputFile)
+  isSwf = inputFile.downcase.end_with?('.swf')
+  isTxt = inputFile.downcase.end_with?('.txt')
+  abort "error: '#{File.basename(inputFile)}' is not a swf or txt file" unless isTxt || isSwf
   bspSwf = File.join(__dir__,'swf','bsp.swf')
   abort 'error: bsp/bsp.swf missing in script directory' unless File.exist?(bspSwf)
 
@@ -34,7 +36,7 @@ else
     ruffle = `which ruffle`.strip
   end
   abort 'error: ruffle is not installed on the system/not in PATH (download: https://ruffle.rs/downloads)' if ruffle.nil? || !File.exist?(ruffle)
-  abort "error: directory '#{outDir}' does not exist" unless File.directory?(outDir)
+  abort "error: directory '#{File.basename(outDir)}' does not exist" unless File.directory?(outDir)
 end
 cryptRb = File.join(__dir__,'crypt.rb')
 abort 'error: crypt.rb not found in script directory' unless File.exist?(cryptRb)
@@ -58,9 +60,12 @@ if options[:blank]
     #0=[0,0,0,0,0,1,-1,-1]
     ===bspend===
   LINES
+elsif isTxt
+  # manual bsp import
+  bspData = File.read(inputFile).split("\n")
 else
   # get bsp data from running bsp.swf
-  parameters = ['--scale','show-all','--no-gui','--filesystem-access-mode','allow',"-PinputLevel=#{swf}"]
+  parameters = ['--scale','show-all','--no-gui','--filesystem-access-mode','allow',"-PinputLevel=#{inputFile}"]
   parameters << '-Pauto=true' if options[:auto]
   parameters << '-PbalancedBSP=false' if options[:unbalanced]
   output,_stderr,_status = Open3.capture3(ruffle,*parameters,bspSwf.to_s)
@@ -80,10 +85,6 @@ else
   end
   abort 'error: bsp data not found in ruffle output (closed too early?)' if bspData == []
 end
-
-# manual bsp import
-# bspData = <<~LINES.split("\n")
-# LINES
 
 # retrieve bsp data
 lineData = []

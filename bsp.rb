4748485a50e9a93263c96e961a72d06018ab687e
WorstCC <worstcc@proton.me>
2026-03-24 13:38:16 -0400

    chore(bsp): remove debug trace

--- bsp.rb
+++ bsp.rb
@@ -104,7 +104,6 @@ bspData.each do |line|
     values = content.split(',').map(&:to_f)
     values[6] = values[6] * 8 unless values[6].negative?
     values[7] = values[7] * 8 unless values[7].negative?
-    puts values
     if mode == :lines
       lineData.concat(values[0..7])
     elsif mode == :waypoints
